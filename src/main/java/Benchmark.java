279c91b7860d4d0aa57c0cab6a26b82c3236fbf1
Teodor Halvadzhiev <Teodor Halvadzhiev>
2016-06-25 16:28:08 +0300

    added benchmark implementation

--- src/main/java/Benchmark.java
+++ src/main/java/Benchmark.java
@@ -1,4 +1,23 @@
 
 public class Benchmark {
+   public static void start(long precision, boolean verbose) {
+      int processors = Runtime.getRuntime().availableProcessors();
+      long oneThread = 1;
+      long[] times = new long[processors];
+      System.out.printf("Starting benchmark for %d digits!\n", precision);
+      for (int i = 1; i <= 2 * processors; i++) {
+         PiCalculator calc = new PiCalculator(precision, i, verbose);
+         long time = System.currentTimeMillis();
+         calc.calcPiAlternative();
+         long finishedAt = System.currentTimeMillis() - time;
+         if (i == 1) {
+            oneThread = finishedAt;
+         }
+         float acceleration = (float) oneThread / (float) finishedAt;
+         System.out.printf("Finished in %d ms, on %d threads, acceleration is %f\n",
+               finishedAt, i, acceleration);
+      }
 
+
+   }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import static Constants.Constants.CLI_BENCHMARK;
 import static Constants.Constants.CLI_OUTPUT_FILE;
 import static Constants.Constants.CLI_PRECISION;
 import static Constants.Constants.CLI_THREAD_COUNT;
@@ -9,26 +10,26 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.apfloat.ApfloatMath;
 
 public class Main {
    public static long precision = 1000000;
    public static int threads = 8;
    public static boolean verbose = false;
    public static String outputFile = "pi-ramanujan.txt";
+   public static boolean benchmark = false;
 
    public static void main(String[] args) {
-      //parseCommandLineInput(args);
+      parseCommandLineInput(args);
 
-      PiCalculator calc = new PiCalculator(precision, threads, verbose);
-      long time = System.currentTimeMillis();
-      Pi pi = calc.calcPiAlternative();
-
-      System.out.println(System.currentTimeMillis() - time);
-      ApfloatMath.pi(precision);
-      time = System.currentTimeMillis();
-      compare(pi.toString(), pi.getPiString());
-      System.out.println(System.currentTimeMillis() - time);
+      if (!benchmark) {
+         PiCalculator calc = new PiCalculator(precision, threads, verbose);
+         long time = System.currentTimeMillis();
+         Pi pi = calc.calcPiAlternative();
+         System.out.printf("Calculated pi to %d digits on %d threads in %d ms\n",
+               precision, threads, (System.currentTimeMillis() - time));
+      } else {
+         Benchmark.start(precision, verbose);
+      }
 
    }
 
@@ -71,6 +72,10 @@ public class Main {
          outputFile = cmd.getOptionValue(CLI_OUTPUT_FILE);
       }
 
+      if (cmd.hasOption(CLI_BENCHMARK)) {
+         benchmark = true;
+      }
+
    }
 
    private static CommandLine getCommandLineParser(String[] args, Options opts) {
@@ -91,7 +96,10 @@ public class Main {
       options.addOption(CLI_VERBOSE, false,
             "determine whether to print thread information or not")
             .addOption(CLI_THREAD_COUNT, true, "number of threads to calculate pi on")
-            .addOption(CLI_PRECISION, true, "number of digits of pi to calculate");
+            .addOption(CLI_PRECISION, true, "number of digits of pi to calculate")
+            .addOption(CLI_OUTPUT_FILE, false, "the file to output the calculated pi to")
+            .addOption(CLI_BENCHMARK, false, "whether to run a benchmark or not");
+
       return options;
    }
 
